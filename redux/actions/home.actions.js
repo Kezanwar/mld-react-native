e4fe03e15dde586a71da7be591547dc213579b24
Kez Anwar <kezanwar@gmail.com>
2022-10-10 16:05:56 +0100

    re structured recommended products into 1 api call

--- redux/actions/home.actions.js
+++ redux/actions/home.actions.js
@@ -4,9 +4,9 @@ import axios from 'axios'
 
 export const getHomeSingleProduct = (id) => async (dispatch) => {
   try {
+    dispatch({ type: HOME.SET_HOME_SINGLE_PRODUCT_LOADING })
     const res = await axios.get(`${env.url}/api/redis/single?id=${id}`)
-    const prod = res.data
-    dispatch({ type: HOME.GET_HOME_SINGLE_PRODUCT, payload: prod })
+    dispatch({ type: HOME.GET_HOME_SINGLE_PRODUCT, payload: { ...res.data } })
   } catch (error) {
     console.log(error)
   }
--- src/features/SingleProduct/components/SingleProductRecommendedProducts.js
+++ src/features/SingleProduct/components/SingleProductRecommendedProducts.js
@@ -1,63 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useContext, useState, useEffect } from 'react'
-import PropTypes from 'prop-types'
+import React, { useContext } from 'react'
 import SingleProductContext from '../SingleProductContext'
-import axios from 'axios'
-import env from '../../../../mld.config'
 import ProductCarousel from '../../ProductCarousel/ProductCarousel'
 import StandardSectionTitle from '../../../components/StandardSectionTitle/StandardSectionTitle'
-import { spacing } from '../../../utils/sizes'
-import { useMemo } from 'react'
 import Loading from '../../../components/Loading/Loading'
-import { useFocusEffect } from '@react-navigation/native'
 
 const SingleProductRecommendedProducts = ({ navigation, stack }) => {
   const context = useContext(SingleProductContext)
   if (!context) return null
-  const forwardProps = { sameScreenNavigate: true, navigation, stack }
-  const [recommendedProducts, setRecommendedProducts] = useState(null)
-  const [recommendedLoading, setRecommendedLoading] = useState(null)
-  const fetchRecommended = async () => {
-    setRecommendedLoading(true)
-    try {
-      const response = await axios.get(`${env.url}/api/redis/single-product/recommended`, {
-        params: {
-          product_category_id: context.categories[0].id,
-          store_id: context.store.id,
-        },
-      })
-      if (response?.data) {
-        setRecommendedProducts(response.data)
-        setRecommendedLoading(false)
-      }
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  useFocusEffect(
-    React.useCallback(() => {
-      if (context?.id === context?.paramsId && !recommendedLoading) fetchRecommended()
-      return () => {
-        setRecommendedProducts(null)
-      }
-    }, [context])
-  )
+  const { store, recommended_products, store_products } = context
+  const forwardProps = { navigation, stack }
 
   return (
     <View style={styles.singleProdRecommendedProducts}>
-      <StandardSectionTitle
-        customStyles={styles.sectionTitleCustomStyles}
-        text={`More from ${context.store.shop_name} `}
-      />
-      {!recommendedLoading && recommendedProducts?.store_products ? (
-        <ProductCarousel {...forwardProps} products={recommendedProducts.store_products} />
+      <StandardSectionTitle customStyles={styles.sectionTitleCustomStyles} text={`More from ${store.shop_name} `} />
+
+      {store_products ? (
+        <ProductCarousel sameScreenNavigate={true} {...forwardProps} products={store_products} />
       ) : (
         <Loading />
       )}
+
       <StandardSectionTitle customStyles={styles.sectionTitleCustomStyles} text="Recommended products" />
-      {!recommendedLoading && recommendedProducts?.recommended_products ? (
-        <ProductCarousel {...forwardProps} products={recommendedProducts.recommended_products} />
+      {recommended_products ? (
+        <ProductCarousel sameScreenNavigate={true} {...forwardProps} products={recommended_products} />
       ) : (
         <Loading />
       )}
